98669f7dd24f892b3006a3358fbe75a5b0b9f576
David Voigt <23502764+bonfiresqaure@users.noreply.github.com>
2021-08-14 13:02:38 +0200

    refactored Config.py

--- config/Config.py
+++ config/Config.py
@@ -14,21 +14,23 @@ class Config (ABC):
     def init(file):
         Config._file = file
         attributes = Config.load()
-        Config._add_attribute(Config, attributes)
+        Config._add_attribute(attributes)
         return
 
     @staticmethod
-    def _add_attribute(base, attribute):
+    def _add_attribute(attribute, base = None):
+        if not base:
+            base = Config
         if len(attribute.keys()) == 1:
-            for key in attribute:
-                setattr(base, key, attribute[key])
+            for _key, _value in attribute.items():
+                setattr(base, _key, _value)
         else:
-            for key in attribute:
-                if isinstance(attribute[key], dict):
-                    Config._add_attribute(base, {key: Attribute()})
-                    Config._add_attribute(getattr(base, key), attribute[key])
+            for _key, _value in attribute.items():
+                if isinstance(_value, dict):
+                    Config._add_attribute({_key: Attribute()}, base)
+                    Config._add_attribute(_value, getattr(base, _key))
                 else:
-                    Config._add_attribute(base, {key: attribute[key]})
+                    Config._add_attribute({_key: _value}, base)
         return
 
     @staticmethod
@@ -37,14 +39,14 @@ class Config (ABC):
             base = Config
         attribute = {}
         members = base.__dict__
-        for _key in members:
+        for _key, _value in members.items():
             if isinstance(members, MappingProxyType):
-                if isinstance(members[_key], Attribute):
+                if isinstance(_value, Attribute):
                     if not key or _key == key:
                         new_base = getattr(base, _key)
                         attribute[_key] = Config._get_attribute(_key, new_base)
             else:
-                attribute[_key] = members[_key]
+                attribute[_key] = _value
         return attribute
 
     @staticmethod
@@ -58,3 +60,8 @@ class Config (ABC):
         with open(Config._file, 'w') as file:
             yaml.dump(attributes, file)
             file.close()
+
+if __name__ == '__main__':
+    Config.init('config.yml')
+    c = Config._get_attribute()
+    print(c)
