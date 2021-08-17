b11f78f979edd97376c26da606f56d1cb8a99261
David Voigt <23502764+bonfiresqaure@users.noreply.github.com>
2021-08-18 01:04:04 +0200

    added methods to Api

--- app/Api.py
+++ app/Api.py
@@ -1,9 +1,10 @@
-from abc import ABC
+from datetime import datetime as dt
+from datetime import timedelta as td
 import requests
 
 
-class Api (ABC):
-    _base_url = None
+class Api:
+    _base_url = None    # default: https://api.coinpaprika.com/
 
     @staticmethod
     def review(response):
@@ -18,40 +19,79 @@ class Api (ABC):
             return response.json()
 
     @staticmethod
-    def init(url):
+    def init(url: str):
         Api._base_url = url
         return
 
     @staticmethod
-    def get_coins():
-        response = requests.get(Api._base_url + 'coins')
+    def get_coin(id: str):
+        url = Api._base_url + 'coins' + (f'/{id}' if id else None)
+        response = requests.get(url)
         return Api.review(response)
 
     @staticmethod
-    def get_exchanges():
-        response = requests.get(Api._base_url + 'exchanges')
+    def get_coin_exchanges(id: str):
+        response = requests.get(Api._base_url + 'coins/' + id + '/exchanges')
         return Api.review(response)
 
     @staticmethod
-    def get_coin(coin_id):
-        response = requests.get(Api._base_url + 'coins/' + coin_id)
+    def get_coin_markets(id: str, quotes: str = 'USD,BTC'):
+        # allowed quote currencies: BTC, ETH, USD, EUR, PLN, KRW, GBP, CAD, JPY, RUB, TRY, NZD, AUD, CHF, UAH, HKD, SGD, NGN, PHP, MXN, BRL, THB, CLP, CNY, CZK, DKK, HUF, IDR, ILS, INR, MYR, NOK, PKR, SEK, TWD, ZAR, VND, BOB, COP, PEN, ARS, ISK
+        params = {'quotes': quotes}
+        response = requests.get(Api._base_url + 'coins/' + id + '/markets', params=params)
+        return Api.review(response)
+
+
+    @staticmethod
+    def get_coin_ohlc_latest(id: str, quotes: str = 'USD,BTC'):
+        # allowed quote currencies: USD, BTC
+        params = {'quotes': quotes}
+        url = Api._base_url + f'coins/{id}/ohlcv/latest'
+        response = requests.get(url, params=params)
         return Api.review(response)
 
     @staticmethod
-    def get_coin_markets(coin_id, params):
-        response = requests.get(Api._base_url + 'coins/' + coin_id + '/markets', params=params)
+    def get_coin_ohlc_range( id: str,
+                             start: str = str(int(dt.timestamp(dt.now() - td(days=7)))),
+                             end: str = str(int(dt.timestamp(dt.now()))),
+                             rows: int = 1,
+                             quotes:str = 'USD,BTC' ):
+        # allowed time values:
+            # RFC3999 (ISO-8601) eg. 2018-02-15T05:15:00Z
+            # Simple date (yyyy-mm-dd) eg. 2018-02-15
+            # Unix timestamp (in seconds) eg. 1518671700
+        # allowed row limit: 1 - 366
+        # allowed quote currencies: USD, BTC
+        params = {'start': start, 'end': end, 'limit': rows, 'quotes': quotes}
+        url = Api._base_url + f'coins/{id}/ohlcv/historical'
+        response = requests.get(url, params=params)
         return Api.review(response)
 
     @staticmethod
-    def get_exchange_markets(exchange_id, params):
-        response = requests.get(Api._base_url + 'exchanges/' + exchange_id + '/markets', params=params)
+    def get_coin_ohlc_today(id: str, quotes: str = 'USD,BTC'):
+        # allowed quote currencies: USD, BTC
+        params = {'quotes': quotes}
+        url = Api._base_url + f'coins/{id}/ohlcv/today'
+        response = requests.get(url, params=params)
         return Api.review(response)
 
     @staticmethod
-    def convert(coin_id, quote, amount):
+    def get_exchange(id: str = None):
+        url = Api._base_url + 'exchanges' + (f'/{id}' if id else None)
+        response = requests.get(url)
+        return Api.review(response)
+
+    @staticmethod
+    def get_exchange_markets(id: str, params: dict = {'quotes':'USD,EUR'}):
+        # allowed quote currencies: BTC, ETH, USD, EUR, PLN, KRW, GBP, CAD, JPY, RUB, TRY, NZD, AUD, CHF, UAH, HKD, SGD, NGN, PHP, MXN, BRL, THB, CLP, CNY, CZK, DKK, HUF, IDR, ILS, INR, MYR, NOK, PKR, SEK, TWD, ZAR, VND, BOB, COP, PEN, ARS, ISK
+        response = requests.get(Api._base_url + 'exchanges/' + id + '/markets', params=params)
+        return Api.review(response)
+
+    @staticmethod
+    def convert(base_id: str, quote_id: str, amount: float = 1):
         params = {
-            'base_currency_id': coin_id,
-            'quote_currency_id': quote,
+            'base_currency_id': base_id,
+            'quote_currency_id': quote_id,
             'amount': amount
         }
         response = requests.get(Api._base_url + 'price-converter', params=params)
